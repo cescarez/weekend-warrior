3f4267fccac44bdcd62d6ab32fafa588dd441c3c
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-25 16:58:39 -0700

    Completed to Green, using a class variable though...

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,20 @@
+class Robot
+  attr_reader :name
+  @@current_serial = "RO807"
 
+  def initialize(name = @@current_serial)
+    @name = name
+    increment_serial_num
+  end
+
+  def reset
+    @name = @@current_serial
+    increment_serial_num
+  end
+
+  def increment_serial_num
+    new_num = @@current_serial.split('RO').last.to_i + 1
+    @@current_serial = "RO#{new_num}"
+  end
+
+end
