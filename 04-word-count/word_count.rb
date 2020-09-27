3c5dbdc09ae6ea812cf2ce81c9e85d02001ad2ea
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-27 16:02:10 -0700

    Green per tests

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,10 @@
+def words(sentence)
+  words_array = sentence.split(' ')
+  words_hash = Hash.new(0)
 
+  words_array.each do |word|
+    words_hash[word.downcase] += 1
+  end
+
+  return words_hash
+end
