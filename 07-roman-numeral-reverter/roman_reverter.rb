10fe9f607897a78e1f5f22656f00be42bcf2c451
Christabel Sebastian <christabel.escarez@gmail.com>
2020-09-29 15:31:18 -0700

    Got to subtractions with >2 chars

--- 07-roman-numeral-reverter/roman_reverter.rb
+++ 07-roman-numeral-reverter/roman_reverter.rb
@@ -3,7 +3,7 @@ VALUES = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000}
 class Roman
 
   def self.reverter(string)
-    match_set = VALUES.keys.flatten
+    match_set = VALUES.keys.flatten.reverse
 
     return ArgumentError, "#{string} contains invalid character(s); cannot translate to Arabic representation." if !string.match?(/#{match_set}/i)
 
@@ -11,8 +11,10 @@ class Roman
     sum = parsed_string.sum { |char| VALUES[char.to_sym] }
 
     if string.match?(/#{match_set.reverse}/)
-      index = string =~ /#{match_set.reverse}/i
-      subtract = VALUES[parsed_string[index].to_sym] * 2
+      if string.length == 2
+        subtract = VALUES[parsed_string[0].to_sym] * 2
+      else
+      end
     end
 
     return sum - subtract
